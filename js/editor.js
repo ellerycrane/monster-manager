d3a36155304d3798e678a6d147039793984663d4
Ellery Crane <Ellery.Crane@dealer.com>
2015-03-24 17:01:56 -0400

    Added some loader logic.

--- js/editor.js
+++ js/editor.js
@@ -4,12 +4,26 @@ App.initialize();
 Editor.initialize(window.flux.actions.updateMonsters);
 
 
-function receiveMessage(event) {
-    console.log("got message!");
-    console.log(event);
-    console.log(event.source);
-    console.log(event.data);
-    event.source.postMessage({test:true, foo:'bar', app:App}, event.origin);
-}
-
-window.addEventListener("message", receiveMessage, false);
+//function receiveMessage(event) {
+//    var eventUrl = event.data.url;
+//    if(eventUrl == null){
+//        console.log("No url to load, aborting");
+//    } else {
+//        $.ajax({
+//            url: eventUrl,
+//            dataType: "text"
+//        }).done(function (data) {
+//            console.log("Loaded data");
+//            eval(data);
+//            //event.source.postMessage({urldata: data}, event.origin);
+//        });
+//    }
+//    //
+//    //console.log("got message!");
+//    //console.log(event);
+//    //console.log(event.source);
+//    //console.log(event.data);
+//    //event.source.postMessage({test:true, foo:'bar', app:App}, event.origin);
+//}
+//
+//window.addEventListener("message", receiveMessage, false);
